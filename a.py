585b5fae8c30d2bc194391baeb7fca4b15543adb
Subekshya Kayastha <subekshyakayastha4@gmail.com>
2022-08-11 11:57:15 +0545

    string in python

--- a.py
+++ a.py
@@ -1,6 +1,12 @@
 # --> It represents a one line comment in python
 
-***
+'''
 It can represent a multi-line comment
 We can write any thing here 
-***
+'''
+
+a = 5 #here, a is a variable which stores/holds integer type value i.e 5
+naam = "Subekshya" #here, naam is a variable which stores/holds string type i.e "Subekshya"
+
+# And in python string are always inside single or double quotes 
+# For example: "subekshya"  OR  "subekshya"

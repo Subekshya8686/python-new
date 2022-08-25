d89784c0d54249d521ed601e4f9b523b9580d463
Subekshya Kayastha <subekshyakayastha4@gmail.com>
2022-08-25 11:17:30 +0545

    new commit

--- claswork.py
+++
@@ -1,6 +0,0 @@
-def sendName(firstName, lastName):
-    return (firstName + " " + lastName )
-    firstName = input("Enter First Name:")
-    secondName = input("Enter Second Name:")
-    
-
---
+++ loop.py
@@ -0,0 +1,21 @@
+#even_num_list=[2,4,6,8,10]
+
+#range(s,s,s) --> start , stop, step
+for i in range(0,10,1):             #i is for variable
+    print(f"The value of i is: {i}")
+
+
+days_list = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+]
+
+for day in days_list:
+     print(f"The single day is: {day} ")
+
+
